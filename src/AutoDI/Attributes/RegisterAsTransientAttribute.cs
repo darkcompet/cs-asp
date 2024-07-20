bdd257c1b16eb9ebf1b8372371be163449860685
DarkCompet <darkcompet@gmail.com>
2024-07-20 19:18:25 +0700

    Add auto dependency injection

---
+++ src/AutoDI/Attributes/RegisterAsTransientAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace Tool.Compet.AutoDI;
+
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Register the class as transient without interface.
+/// </summary>
+public class RegisterAsTransient : AutoDIRegistrationAttribute {
+	public static readonly string FullName = typeof(RegisterAsTransient).FullName!;
+
+	public RegisterAsTransient() {
+		this.serviceLifetime = ServiceLifetime.Transient;
+	}
+}
