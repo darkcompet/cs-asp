2480a799c303a6bfda008c0b7dbe1095c517f284
DarkCompet <darkcompet@gmail.com>
2025-02-22 19:50:14 +0700

    Allow null value in required enum attribute

--- src/Validation/RequiredEnumDk.cs
+++ src/Validation/RequiredEnumDk.cs
@@ -5,8 +5,11 @@ using System.ComponentModel.DataAnnotations;
 /// <summary>
 /// Require the value is one of the enum values.
 /// </summary>
-public class RequiredEnumDk : RequiredAttribute {
+public class RequiredEnumDk(bool allowNull = false) : RequiredAttribute {
 	public override bool IsValid(object? value) {
+		if (allowNull && value is null) {
+			return true;
+		}
 		return DkValidators.IsValidEnumValue(value);
 	}
 }
